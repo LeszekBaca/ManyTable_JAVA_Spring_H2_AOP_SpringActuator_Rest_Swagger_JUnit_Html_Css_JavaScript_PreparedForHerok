7efb9e133256d097abbc3feb2286ec8a00f91ed4
Leszek Baca <leszek.baca@op.pl>
2023-07-24 21:33:12 +0200

    Reformat code

--- src/main/java/com/project/advice/LoggingAspect.java
+++ src/main/java/com/project/advice/LoggingAspect.java
@@ -12,18 +12,14 @@ public class LoggingAspect {
         System.out.println("****LoggingAspect.logGetComponentById() : " + joinPoint.getSignature().getName());
         System.out.println("****LoggingAspect.logGetComponentById() : className: " + joinPoint.getTarget().getClass().toString());
     }
-
     @After("execution(* com.project.service.service_action.ServiceActionImpl.addServiceAction(..))")
     public void logAddServiceAction(JoinPoint joinPoint) {
         System.out.println("****LoggingAspect.logAddServiceAction() : " + joinPoint.getSignature().getName());
         System.out.println("****LoggingAspect.logAddServiceAction() : className: " + joinPoint.getTarget().getClass().toString());
     }
-
-
     @After("execution(* com.project.service.warehouse.WarehouseServiceImpl.getWareHouseById(..))")
     public void logGetWareHouseById(JoinPoint joinPoint) {
         System.out.println("****LoggingAspect.logGetWareHouseById() : " + joinPoint.getSignature().getName());
         System.out.println("****LoggingAspect.logGetWareHouseById() : className: " + joinPoint.getTarget().getClass().toString());
     }
-
 }
